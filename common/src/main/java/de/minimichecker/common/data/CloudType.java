0b02cb5920f10bb0b95604d84e5d79e275d788fc
Bluplayz <bluplayz98@gmail.com>
2020-09-20 19:17:07 +0200

    Initial Commit

---
+++ common/src/main/java/de/minimichecker/common/data/CloudType.java
@@ -0,0 +1,43 @@
+package de.minimichecker.common.data;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Getter
+@Setter
+public class CloudType {
+
+    private String name = "";
+    private Type type = Type.BUKKIT;
+    private int minOnlineServers = 0;
+    private int maxOnlineServers = 0;
+    private int portStart = 0;
+    private int portEnd = 0;
+    private int priority = 0;
+    private int memory = 0;
+    private boolean staticService = false;
+    private String templatePath = "/" + this.name + "/";
+    private boolean startOnlyOnSpecifiedHosts = false;
+    private Collection<String> hosts = new ArrayList<>();
+    private Collection<String> inheritances = new ArrayList<>();
+    private Collection<String> proxyFallbackPriorities = new ArrayList<>();
+    private Collection<String> javaParameters = new ArrayList<>();
+    private Collection<String> spigotParameters = new ArrayList<>();
+
+    public enum Type {
+        BUKKIT, BUNGEECORD, TEMPLATE
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        return ((CloudType) object).getName().equalsIgnoreCase(this.getName());
+    }
+
+    @Override
+    public String toString() {
+        return this.getName();
+    }
+}
--- server-coordinator/src/main/java/de/minimichecker/coordinator/ServerCoordinator.java
+++ server-coordinator/src/main/java/de/minimichecker/coordinator/ServerCoordinator.java
@@ -6,19 +6,23 @@ import com.google.inject.Injector;
 import com.google.inject.Singleton;
 import de.minimichecker.common.command.CommandRegister;
 import de.minimichecker.common.command.ConsoleInputReader;
+import de.minimichecker.common.config.FileRootConfig;
 import de.minimichecker.common.config.RedisConfig;
 import de.minimichecker.common.database.redis.RedisConnection;
 import de.minimichecker.common.logging.Logger;
 import de.minimichecker.common.logging.LoggingInitializer;
-import de.minimichecker.common.network.packet.packets.HelloWorldPacket;
 import de.minimichecker.common.network.packet.repository.PacketListenerRepository;
 import de.minimichecker.common.network.packet.sender.PacketSender;
+import de.minimichecker.common.setup.FileRootSetup;
+import de.minimichecker.common.setup.RedisCredentialsSetup;
 import de.minimichecker.common.util.LibraryUtils;
 import de.minimichecker.coordinator.module.ModuleSettings;
 import lombok.Getter;
 
 import java.io.File;
+import java.net.InetAddress;
 import java.net.URISyntaxException;
+import java.net.UnknownHostException;
 
 @Singleton
 @Getter
@@ -94,6 +98,15 @@ public final class ServerCoordinator {
     @Inject
     private PacketListenerRepository packetListenerRepository;
 
+    @Inject
+    private RedisCredentialsSetup redisCredentialsSetup;
+
+    @Inject
+    private FileRootConfig fileRootConfig;
+
+    @Inject
+    private FileRootSetup fileRootSetup;
+
     private Logger logger;
 
     @Inject
@@ -108,12 +121,26 @@ public final class ServerCoordinator {
         this.getLoggingInitializer().init(new File(ServerCoordinator.getRootDirectory(), "logs"));
         this.logger = this.getLoggingInitializer().getLogger();
 
+        this.getLogger().info("Starting ServerCoordinator...");
+
         // Initialize ConsoleInputReader
         this.getConsoleInputReader().init();
 
+        // Check for RootDirectory
+        File configFile = new File(ServerCoordinator.getRootDirectory(), "rootdir.properties");
+        if (!configFile.exists()) {
+            this.getFileRootSetup().startSetup(configFile);
+        }
+
+        this.getFileRootConfig().load(configFile);
+
         // Connect to Redis
-        File configFile = new File(ServerCoordinator.getRootDirectory(), "redis.properties");
-        this.getRedisConfig().createIfNotExist(configFile);
+        File fileRootDirectory = new File(this.getFileRootConfig().getRootFileDirectory());
+        configFile = new File(new File(fileRootDirectory, "Configs"), "redis.properties");
+        if (!configFile.exists()) {
+            this.getRedisCredentialsSetup().startSetup(configFile);
+        }
+
         this.getRedisConfig().load(configFile);
 
         this.getRedisConnection().init(this.getRedisConfig().getHost(), this.getRedisConfig().getPort(), this.getRedisConfig().getPassword());
@@ -124,6 +151,15 @@ public final class ServerCoordinator {
         }
 
         this.getLogger().info("Connected to Redis.");
+
+        this.getLogger().info("ServerCoordinator is now started.");
+
+        /*
+        try {
+            this.getLogger().info("Hostname=" + InetAddress.getLocalHost().getHostName());
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
 
         HelloWorldPacket helloWorldPacket = new HelloWorldPacket();
         helloWorldPacket.setMessage("HelloWorld2");
@@ -138,5 +174,6 @@ public final class ServerCoordinator {
             HelloWorldPacket packet = resultPacket;
             System.out.println("Packet came back! " + packet.toString());
         }, "testChannel");
+        */
     }
 }

29aca7034fb0445ef9467ad6fb23a2db05b22303
Bluplayz <bluplayz98@gmail.com>
2020-09-21 11:37:33 +0200

    added service connect & disconnect

---
+++ common/src/main/java/de/blu/common/network/packet/packets/ServiceDisconnectedPacket.java
@@ -0,0 +1,29 @@
+package de.blu.common.network.packet.packets;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Map;
+import java.util.UUID;
+
+@Getter
+@Setter
+public class ServiceDisconnectedPacket extends Packet {
+
+    private String serviceName;
+    private UUID serviceIdentifier;
+
+    @Override
+    public Map<String, String> write(Map<String, String> data) {
+        data.put("serviceName", this.getServiceName());
+        data.put("serviceIdentifier", this.getServiceIdentifier().toString());
+
+        return data;
+    }
+
+    @Override
+    public void read(Map<String, String> content) {
+        this.setServiceName(content.get("serviceName"));
+        this.setServiceIdentifier(UUID.fromString(content.get("serviceIdentifier")));
+    }
+}
---
+++ server-coordinator/src/main/java/de/blu/coordinator/listener/PacketHandler.java
@@ -0,0 +1,46 @@
+package de.blu.coordinator.listener;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import de.blu.common.logging.Logger;
+import de.blu.common.network.packet.packets.RequestCloudTypesPacket;
+import de.blu.common.network.packet.packets.ServiceConnectedPacket;
+import de.blu.common.network.packet.repository.PacketListenerRepository;
+import de.blu.common.network.packet.sender.PacketSender;
+import de.blu.common.repository.CloudTypeRepository;
+import lombok.Getter;
+
+@Singleton
+@Getter
+public final class PacketHandler {
+
+    @Inject
+    private PacketSender packetSender;
+
+    @Inject
+    private PacketListenerRepository packetListenerRepository;
+
+    @Inject
+    private CloudTypeRepository cloudTypeRepository;
+
+    @Inject
+    private Logger logger;
+
+    public void registerAll() {
+        // Register default for Callbacks
+        this.getPacketListenerRepository().registerListener((packet, hadCallback) -> {
+        }, "CallbackChannel");
+
+        this.getPacketListenerRepository().registerListener((packet, hadCallback) -> {
+            if (packet instanceof RequestCloudTypesPacket) {
+                ((RequestCloudTypesPacket) packet).setCloudTypes(this.getCloudTypeRepository().getCloudTypes());
+                packet.sendBack();
+            }
+        }, "RequestCloudTypes");
+
+        this.getPacketListenerRepository().registerListener((packet, hadCallback) -> {
+            ServiceConnectedPacket serviceConnectedPacket = (ServiceConnectedPacket) packet;
+            this.getLogger().info("Service connected: " + serviceConnectedPacket.getServiceName() + " (" + serviceConnectedPacket.getServiceIdentifier() + ")");
+        }, "ServiceConnected");
+    }
+}

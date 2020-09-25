83240e647b2084da4346d7004bf5c08450f44062
Bluplayz <bluplayz98@gmail.com>
2020-09-25 18:43:37 +0200

    added shutdown and help command

---
+++ server-starter/src/main/java/de/blu/starter/command/HelpCommand.java
@@ -0,0 +1,29 @@
+package de.blu.starter.command;
+
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import de.blu.common.command.CommandRegister;
+import de.blu.common.command.data.Command;
+import de.blu.common.command.data.CommandData;
+import de.blu.common.command.data.CommandExecutor;
+import lombok.Getter;
+
+import java.util.Arrays;
+
+@Singleton
+@Command(name = "help", aliases = "?")
+@Getter
+public final class HelpCommand extends CommandExecutor {
+
+    @Inject
+    private CommandRegister commandRegister;
+
+    @Override
+    public void execute(String label, String[] args) {
+        System.out.println("&bCommands:");
+
+        for (CommandData command : this.getCommandRegister().getConsoleCommandHandler().getCommands()) {
+            System.out.println("- &e" + command.getName() + " &r(Aliases: &e" + Arrays.toString(command.getAliases()) + "&r)");
+        }
+    }
+}

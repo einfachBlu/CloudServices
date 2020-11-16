1d314bf5fd5e79e1dd60f4fb56b1c24d1715aab9
Fabien Blume <fabien.blume@hotmail.com>
2020-11-16 10:01:31 +0100

    Added WebPanel Module for future

--- common/src/main/java/de/blu/common/network/packet/packets/CloudCoordinatorReloadPacket.java
+++ common/src/main/java/de/blu/common/network/packet/packets/CloudCoordinatorReloadPacket.java
@@ -4,18 +4,7 @@ import de.blu.common.network.packet.Packet;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.Map;
-
 @Getter
 @Setter
 public class CloudCoordinatorReloadPacket extends Packet {
-
-    @Override
-    public Map<String, String> write(Map<String, String> data) {
-        return data;
-    }
-
-    @Override
-    public void read(Map<String, String> content) {
-    }
 }
